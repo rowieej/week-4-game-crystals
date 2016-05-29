963f5bb0a13fc6ab65d55ceb5ac80239cb4827b8
rowieej <rowena.jaico@gmail.com>
2016-05-28 22:01:14 -0400

    initial site files with content

---
+++ assets/javascript/game.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+//set up all random values for goal number and each gem
+var values = {
+			goal: Math.floor(Math.random() * 120) + 19, 
+			yellow: Math.floor(Math.random() * 12) + 1,
+			purple: Math.floor(Math.random() * 12) + 1,
+			ruby: Math.floor(Math.random() * 12) + 1,
+			blue: Math.floor(Math.random() * 12) + 1,
+			score: 0,
+};
+//set up value for wins/losses for counter		
+var wins = 0;
+var losses = 0;
+
+console.log(values);
+
+$(".targetNumber").append("Goal Number: " + values.goal);
+$(".userScore").append("Your Score: " + values.score);
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
